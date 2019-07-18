9e3f957b522a0a879f44f1d599c7f910b1d09277
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2019-07-18 20:34:14 +0530

    Create swapp.c
    swap without using temp

---
+++ swapp.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a=10, b=20;
+    
+    a=a+b;  //a=30
+    b=a-b;  //b=10
+    a=a-b;  //a=20
+    
+    printf("\n Value of A:%d",a);
+    printf("\n Value of B:%d",b);
+    
+}
+
+
+
+#include<stdio.h>
+int main()
+{
+    int a=10, b=20;
+    
+    a=a^b;  //a=30
+    b=a^b;  //b=10
+    a=a^b;  //a=20
+    
+    printf("\n Value of A:%d",a);
+    printf("\n Value of B:%d",b);
+    
+}
